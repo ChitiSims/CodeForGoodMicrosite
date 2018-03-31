a004ee5ed8f1a1b0dc45a2d13a350c52c478efcd
Yoda Alemu <yalemu@dhcp-18-189-76-229.dyn.MIT.EDU>
2018-03-31 04:21:47 -0400

    output page

--- output_script.js
+++ output_script.js
@@ -1,6 +1,4 @@
-
-// Initialize Firebase
-const configure = {
+var configuration = {
 apiKey: "AIzaSyBjGBLVPxiEzET1mqtZebJgXwsbVQE_3Ck",
 authDomain: "codeforgoodsite.firebaseapp.com",
 databaseURL: "https://codeforgoodsite.firebaseio.com",
@@ -8,35 +6,8 @@ projectId: "codeforgoodsite",
 storageBucket: "codeforgoodsite.appspot.com",
 messagingSenderId: "824518102248"
 };
-firebase.initializeApp(configure);
-let database = firebase.database();
-let root = database.ref();
+firebase.initializeApp(configuration);
 
-//console.log(root);
-//database.child()
-
-
-// root.once('value').then(successFunction, errorFunction)
-
-
-// root.once('value').then(function(data) {
-//   data.val();
-// }, function(err){
-
-// });
-
-
-/*
-Copyright (C) 2016 Garry Lachman garry@lachman.co under GNU LGPL
-https://rev.proxies.online
-This library is free software; you can redistribute it and/or modify it under the terms of the
-GNU Lesser General Public License version 2.1, as published by the Free Software Foundation.
-This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
-without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-See the GNU Lesser General Public License for more details.
-
-based on https://github.com/hannesgassert/countrynames
-*/
 (function(){
 	var countriesData = {
 		"AFGHANISTAN": "AF",
@@ -322,3 +293,89 @@ based on https://github.com/hannesgassert/countrynames
 	window.CountryCode = CountryCode;
 
 })();
+
+
+
+function select(){
+
+let country =  ["--Select a country--","Afghanistan", "Åland Islands", "Albania", "Algeria", "American Samoa", "Andorra", "Angola", "Anguilla", "Antarctica", "Antigua And Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia, Plurinational State Of", "Bonaire, Sint Eustatius And Saba", "Bosnia And Herzegovina", "Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory", "Brunei Darussalam", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central African Republic", "Chad", "Chile", "China", "Christmas Island", "Cocos (keeling) Islands", "Colombia", "Comoros", "Congo", "Congo, The Democratic Republic Of The", "Cook Islands", "Costa Rica", "Côte D'ivoire", "Croatia", "Cuba", "Curaçao", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Falkland Islands (malvinas)", "Faroe Islands", "Fiji", "Finland", "France", "French Guiana", "French Polynesia", "French Southern Territories", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guernsey", "Guinea", "Guinea-bissau", "Guyana", "Haiti", "Heard Island And Mcdonald Islands", "Holy See (vatican City State)", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran, Islamic Republic Of", "Iraq", "Ireland", "Isle Of Man", "Israel", "Italy", "Jamaica", "Japan", "Jersey", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Korea, Democratic People's Republic Of", "Korea, Republic Of", "Kuwait", "Kyrgyzstan", "Lao People's Democratic Republic", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Macao", "Macedonia, The Former Yugoslav Republic Of", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico", "Micronesia, Federated States Of", "Moldova, Republic Of", "Monaco", "Mongolia", "Montenegro", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Niue", "Norfolk Island", "Northern Mariana Islands", "Norway", "Oman", "Pakistan", "Palau", "Palestine, State Of", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Pitcairn", "Poland", "Portugal", "Puerto Rico", "Qatar", "Réunion", "Romania", "Russian Federation", "Rwanda", "Saint Barthélemy", "Saint Helena, Ascension And Tristan Da Cunha", "Saint Kitts And Nevis", "Saint Lucia", "Saint Martin (french Part)", "Saint Pierre And Miquelon", "Saint Vincent And The Grenadines", "Samoa", "San Marino", "Sao Tome And Principe", "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Sint Maarten (dutch Part)", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Georgia And The South Sandwich Islands", "South Sudan", "Spain", "Sri Lanka", "Sudan", "Suriname", "Svalbard And Jan Mayen", "Swaziland", "Sweden", "Switzerland", "Syrian Arab Republic", "Taiwan, Province Of China", "Tajikistan", "Tanzania, United Republic Of", "Thailand", "Timor-leste", "Togo", "Tokelau", "Tonga", "Trinidad And Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks And Caicos Islands", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "United States Minor Outlying Islands", "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela, Bolivarian Republic Of", "Viet Nam", "Virgin Islands, British", "Virgin Islands, U.s.", "Wallis And Futuna", "Western Sahara", "Yemen", "Zambia", "Zimbabwe"];
+
+let myDiv = document.getElementById("yo")
+
+//Create and append select list
+let selectList = document.createElement("select");
+selectList.id = "added_country";
+myDiv.appendChild(selectList);
+
+//Create and append the options
+for (let i = 0; i < country.length; i++) {
+    let option = document.createElement("option");
+    option.value = country[i];
+    option.text = country[i];
+    selectList.appendChild(option);
+}
+}
+
+function display(){
+	let x = document.getElementById("country").value
+		return xx
+}
+
+function get_shade_values(country_dict){
+    let maxval = 0
+    for (var key in country_dict){
+        if (country_dict[key] >= maxval){
+            maxval = country_dict[key]
+        }
+    }
+    
+    var shade_vals = {}
+    for (var key in country_dict){
+        shade_vals[key] = country_dict[key]/maxval
+    }
+    return shade_vals
+}
+
+function update_map(code, shade_val){
+	console.log(code)
+	let target = document.getElementById(code);
+	target.style.fill = 'green';
+    target.style.fillOpacity = shade_val;
+
+}	
+
+
+function read_db(event){
+	let database = firebase.database().ref().child(event);
+	database.on('value', function(snapshot){
+		let data = snapshot.val();
+		var country_dict = data["countries"];
+        console.log(country_dict)
+        var shade_dict = get_shade_values(country_dict)
+        console.log(shade_dict)
+        for (var key in shade_dict){
+            if (shade_dict[key] != 0){
+                if (shade_dict[key] < 0.3){
+                    update_map(key, 0.3)
+                }
+                else if (shade_dict[key] > 0.9){
+                    update_map(key, 0.9)
+                }
+                else{
+                    update_map(key, shade_dict[key]);
+                }
+            }
+        }
+	})
+}
+
+window.addEventListener("load", main);
+
+
+function main(){
+	let selections = []
+	let country = new CountryCode();
+    
+    read_db('event5')
+    
+}
