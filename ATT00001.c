5693047d151ff83d620b17d96956ece37a966be1
ChitiSims <chiti@mit.edu>
2018-04-05 21:28:45 -0400

    changed name in prep for deploy(ment)?

---
+++ ATT00001.c
@@ -0,0 +1,5 @@
+___________________________________________________
+"Sponge-Talk - because who the hell is Kevin Lang?"
+
+Sponge-talk@mit.edu ---- For Archives & Subscription:
+https://mailman.mit.edu/mailman/listinfo/sponge-talk
